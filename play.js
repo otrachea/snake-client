9591cb6e0a133c91fc08e926a75ff7c760dae772
otrachea <francesca.ho459@gmail.com>
2022-03-16 17:12:58 +0000

    Can now exit client with ctrl+c

--- play.js
+++ play.js
@@ -1,4 +1,31 @@
 const { connect } = require("./client");
 
+const handleUserInput = (data) => {
+  if (data === "\u0003") {
+    console.log("Connection from server terminated");
+    process.exit();
+  }
+
+};
+
+
+const setupInput = function () {
+
+  // process.on('SIGINT', () => {
+  //   console.log('Hey Boss I just Received SIGINT.');
+  // });
+
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+
+  stdin.on("data", handleUserInput)
+
+  return stdin;
+};
+
+
 console.log("Connecting ...");
 connect();
+setupInput();
