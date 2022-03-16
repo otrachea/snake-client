8e7239b999c2e4d741faf04dde5f978521d52285
otrachea <francesca.ho459@gmail.com>
2022-03-16 17:22:21 +0000

    Snake can now move

--- input.js
+++ input.js
@@ -1,17 +1,23 @@
+let connection;
+
 const handleUserInput = (data) => {
   if (data === "\u0003") {
     console.log("Connection from server terminated");
     process.exit();
   }
 
+  if (['w', 'a', 's', 'd'].includes(data)) {
+    connection.write(`Move: ${data}`);
+  }
 };
 
-const setupInput = function () {
+const setupInput = function (conn) {
 
   // process.on('SIGINT', () => {
   //   console.log('Hey Boss I just Received SIGINT.');
   // });
 
+  connection = conn;
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding("utf8");
