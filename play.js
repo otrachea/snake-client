2a143d605fdbf4d9e81c5b659bc99a49cd950b51
otrachea <francesca.ho459@gmail.com>
2022-03-16 17:16:50 +0000

    Refactor code

--- play.js
+++ play.js
@@ -1,31 +1,7 @@
 const { connect } = require("./client");
-
-const handleUserInput = (data) => {
-  if (data === "\u0003") {
-    console.log("Connection from server terminated");
-    process.exit();
-  }
-
-};
-
-
-const setupInput = function () {
-
-  // process.on('SIGINT', () => {
-  //   console.log('Hey Boss I just Received SIGINT.');
-  // });
-
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding("utf8");
-  stdin.resume();
-
-  stdin.on("data", handleUserInput)
-
-  return stdin;
-};
-
+const { setupInput } = require("./input");
 
 console.log("Connecting ...");
 connect();
+
 setupInput();
