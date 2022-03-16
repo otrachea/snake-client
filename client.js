bc30a02f659c79887bd13228ca24bdb620b704d5
otrachea <francesca.ho459@gmail.com>
2022-03-16 15:26:09 +0000

    Name appears on server

---
+++ client.js
@@ -0,0 +1,25 @@
+const net = require("net");
+
+// establishes a connection with the game server
+const connect = function () {
+  const conn = net.createConnection({
+    host: "localhost", // IP address here,
+    port: 50541// PORT number here,
+  });
+
+  // interpret incoming data as text
+  conn.setEncoding("utf8");
+
+  conn.on("connect", () => {
+    console.log("Successfully connected to game server");
+    conn.write("Name: TRA");
+  })
+
+  conn.on("data", (data) => {
+    console.log(data.toString());
+  });
+
+  return conn;
+};
+
+module.exports = { connect };
